b513aa2bfce386be9406d2e4c14f09d490a86409
Ropafadzo shumba <Ropafadzo@dicomm.co.zw>
2019-12-03 16:23:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
  console.log(`Server running on port ${port}`);
